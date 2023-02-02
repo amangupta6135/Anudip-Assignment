4fcba04f51f818f53f2a80d2fcc9d617edd8f999
amangupta6135 <120014284+amangupta6135@users.noreply.github.com>
2023-02-02 17:14:47 +0530

    Add files via upload

---
+++ projectAnudip/src/projectAnudip/HashSetElement.java
@@ -0,0 +1,24 @@
+package projectAnudip;
+
+import java.util.HashSet;
+
+public class HashSetElement {
+
+	public static void main(String[] args) {
+	
+		   // Create a empty hash set
+	     HashSet h_set = new HashSet();
+	   // use add() method to add values in the hash set
+	          h_set.add("Red");
+	          h_set.add("Green");
+	          h_set.add("Black");
+	          h_set.add("White");
+	          h_set.add("Pink");
+	          h_set.add("Yellow");
+	    System.out.println("Original Hash Set: " + h_set);
+	    System.out.println("Size of the Hash Set: " + h_set.size());
+	
+
+	}
+
+}
---
+++ projectAnudip/src/projectAnudip/Mapping.java
@@ -0,0 +1,23 @@
+package projectAnudip;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapping {
+
+	public static void main(String[] args) {
+		  Map map = new HashMap();
+	        
+          // Adding key-values
+        map.put(1, "Amit");
+        map.put(5, "Rahul");
+        map.put(2, "Jai");
+        map.put(6, "Amit");
+        
+          // using the method
+        System.out.println("Size of the map is : " + map.size());
+                          
+
+	}
+
+}
